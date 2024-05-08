5e9b43eb7408e368b04fdeaf2706738fa41ff63c
Kostas stefanidis <kstefanidis48@gmail.com>
2024-05-08 13:07:36 +0000

    Add unit tests for pop method

--- src/data_structures/ArrayList.py
+++ src/data_structures/ArrayList.py
@@ -22,8 +22,10 @@ class ArrayList(Generic[T]):
             self.length = 1
             self.array = [item] + [None for _ in range(self.capacity - 1)]
 
+
     def __len__(self) -> int:
         return self.length
+
 
     def __getitem__(self, index: int):
         if self.length == 0 or index > self.length - 1 or -index > self.length:
@@ -31,21 +33,26 @@ class ArrayList(Generic[T]):
 
         return self.array[index]
 
+
     def __setitem__(self, index: int, value):
         if self.length == 0 or index > self.length - 1 or -index > self.length:
             raise IndexError('ArrayList index out of bounds')
 
         self.array[index] = value
 
+
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}: {self.array}"
 
     def __str__(self) -> str:
-        return str([elem for elem in self.array if elem is not None])
+        # return str([elem for elem in self.array if elem is not None])
+        return str(self.get_array())
+
 
     def __iter__(self):
         self.counter = 0
         return self
+
 
     def __next__(self) -> T:
         if self.length == 0 or self.counter >= self.length:
@@ -55,10 +62,12 @@ class ArrayList(Generic[T]):
         self.counter += 1
         return next_value
 
+
     def __eq__(self, other) -> bool:
         if not isinstance(other, ArrayList):
             raise TypeError(f"Cannot compare {self.__class__.__name__} object to {type(other)}")
         return self.array == other.array
+
 
     def _reallocate_array(self):
         self.capacity = self.capacity * 2
@@ -70,6 +79,10 @@ class ArrayList(Generic[T]):
             new_array[i] = self.array[i]
 
         self.array = new_array
+        
+    
+    def get_array(self):
+        return self.array[:self.length]
 
 
     def push(self, item) -> None:
@@ -81,19 +94,22 @@ class ArrayList(Generic[T]):
 
 
     def pop(self, idx: int = None) -> T | None:
+        print(self.length)
         if self.length == 0:
             raise IndexError('Cannot pop from empty list')
 
+        # pop last item in the ArrayList (like poping from a stack).
+        # No items need to be moved in this case
         if idx is None:
             idx = self.length - 1
-            print(type(idx), idx)
-
+        
         if idx > self.length - 1:
             raise IndexError('ArrayList index out of range')
 
         item = self.array[idx]
         self.array[idx] = None
 
+        # In case an item is popped from the middle of the list, 
         if idx < self.length - 1:
             for i in range(idx, self.length - 1):
                 self.array[i] = self.array[i+1]
@@ -101,6 +117,7 @@ class ArrayList(Generic[T]):
             self.array[self.length - 1] = None
 
         self.length -= 1
+        print(self.length)
         return item
 
 
@@ -115,7 +132,7 @@ class ArrayList(Generic[T]):
 
         self.array[0] = item
 
-
+    # deque is the same as pop(0)
     def deque(self) -> T | None:        
         item = self.array[self.length - 1] if self.length != 0 else None
 
--- tests/test_ArrayList.py
+++ tests/test_ArrayList.py
@@ -1,12 +1,7 @@
-from typing import Literal
 import pytest
 from src.data_structures.ArrayList import ArrayList
 
-
-class TestArrayList():
-    array_list1 = ArrayList(1)
-    array_list2 = ArrayList(2)
-    
+class TestArrayList():    
     @pytest.mark.parametrize(
         "array_list_init, expected_length, expected_capacity",
         [   
@@ -17,16 +12,9 @@ class TestArrayList():
             (list(range(17)), 17, 17),
             (1, 1, 4),
             (None, 0, 4),
-        ],
-        # ids=[
-        #     "length=5 - index=5", 
-        #     "length=5 - index=6", 
-        #     "length=2 - index=2",
-        #     "length=1 - index=1",
-        #     "length=0 - index=0"
-        # ]
+        ]
     )
-    def test_init(self, array_list_init: list[int] | None | Literal[1], expected_length: Literal[2] | Literal[5] | Literal[7] | Literal[9] | Literal[17] | Literal[1] | Literal[0], expected_capacity: Literal[2] | Literal[5] | Literal[7] | Literal[9] | Literal[17] | Literal[4]):
+    def test_init(self, array_list_init, expected_length, expected_capacity):
         array_list = ArrayList(array_list_init)
         assert array_list.length == expected_length
         assert array_list.capacity == expected_capacity
@@ -47,7 +35,7 @@ class TestArrayList():
             (list(range(4)), -4, 0)
         ],
     )
-    def test_getitem(self, array_list_init: list[int] | Literal[1], index: Literal[0] | Literal[1] | Literal[3] | Literal[8] | Literal[16] | Literal[-1] | Literal[-2] | Literal[-3] | Literal[-4], expected_value: Literal[0] | Literal[1] | Literal[3] | Literal[8] | Literal[16] | Literal[2]):
+    def test_getitem(self, array_list_init, index, expected_value):
         array_list = ArrayList(array_list_init)
         assert array_list[index] == expected_value
     
@@ -64,7 +52,7 @@ class TestArrayList():
             (None, -1)
         ],
     )
-    def test_getitem_indexError(self, array_list_init: list[int] | None | Literal[1], index: Literal[2] | Literal[-3] | Literal[-18] | Literal[1] | Literal[-2] | Literal[0] | Literal[-1]):
+    def test_getitem_indexError(self, array_list_init, index):
         array_list = ArrayList(array_list_init)
         with pytest.raises(IndexError):
             _ = array_list[index]
@@ -80,7 +68,7 @@ class TestArrayList():
             ([1,2,3,4,5], -5, 9, [9,2,3,4,5]),
         ]
     )
-    def test_setitem(self, array_list_init: list[int] | Literal[1], index: Literal[0] | Literal[-1] | Literal[1] | Literal[-5], value: Literal[9] | Literal[0] | Literal[8], expected_array_list: list[int] | Literal[8]):
+    def test_setitem(self, array_list_init, index, value, expected_array_list):
         array_list = ArrayList(array_list_init)
         array_list[index] = value
         assert array_list == ArrayList(expected_array_list)
@@ -97,7 +85,7 @@ class TestArrayList():
             (None, 10)
         ]
     )
-    def test_setitem_indexError(self, array_list_init: list[int] | None | Literal[1], index: Literal[5] | Literal[6] | Literal[2] | Literal[1] | Literal[11] | Literal[0] | Literal[10]):
+    def test_setitem_indexError(self, array_list_init, index):
         array_list = ArrayList(array_list_init)
         with pytest.raises(IndexError):
             array_list[index] = 0
@@ -116,7 +104,7 @@ class TestArrayList():
             (None, None, True)
         ]
     )
-    def test_equal(self, array_list_init_1: list[int] | None | Literal[1], array_list_init_2: list[int] | None | Literal[1] | Literal[5], expected_equal: bool):
+    def test_equal(self, array_list_init_1, array_list_init_2, expected_equal):
         array_list_1 = ArrayList(array_list_init_1)
         array_list_2 = ArrayList(array_list_init_2)
         assert (array_list_1 == array_list_2) == expected_equal
@@ -133,7 +121,7 @@ class TestArrayList():
             ([0,8,14,9], ArrayList([1,3])),
         ]
     )
-    def test_equal_typeError(self, object_1: ArrayList[int] | ArrayList[None] | list[int], object_2: list[int] | ArrayList[None] | ArrayList[int]):
+    def test_equal_typeError(self, object_1, object_2):
         with pytest.raises(TypeError):
             object_1 == object_2
     
@@ -154,10 +142,42 @@ class TestArrayList():
             (list(range(2)), list(range(17)), 19, 32),
         ]
     )
-    def test_push(self, array_list_init: None | list[int], items_to_push: list[int], expected_length: Literal[0] | Literal[1] | Literal[4] | Literal[5] | Literal[8] | Literal[9] | Literal[19], expected_capacity: Literal[4] | Literal[8] | Literal[16] | Literal[32]):
+    def test_push(self, array_list_init, items_to_push, expected_length, expected_capacity):
         array_list = ArrayList(array_list_init)
         for item in items_to_push:
             array_list.push(item)
 
         assert array_list.length == expected_length
         assert array_list.capacity == expected_capacity
+
+
+    @pytest.mark.parametrize(
+        "array_list_init, index, expected_array, expected_returned_item",
+        [
+            ([0,1,2,3,4], None, [0,1,2,3], 4),
+            ([0,1,2,3,4], 0, [1,2,3,4], 0),
+            ([0,1,2,3,4], 2, [0,1,3,4], 2),
+            (list(range(10)), None, list(range(9)), 9),
+        ]
+    )
+    def test_pop(self, array_list_init, index, expected_array, expected_returned_item):
+        array_list = ArrayList(array_list_init)
+        item = array_list.pop(index)
+        assert item == expected_returned_item
+        assert array_list.get_array() == expected_array
+
+
+    @pytest.mark.parametrize(
+        "array_list_init, index",
+        [
+            (None, None),
+            (None, 0),
+            (None, 6),
+            ([0,1,2,3,4,5], 6),
+            ([1,2,3], 3)
+        ]
+    )
+    def test_pop_index_error(self, array_list_init, index):
+        array_list = ArrayList(array_list_init)
+        with pytest.raises(IndexError):
+            _ = array_list.pop(index)
